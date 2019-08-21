0b932a2f323db1be253b8d0fc4a3e396692d1fb8
Mr.Nav <mr.nav90@gmail.com>
2019-08-21 23:41:23 +0700

    init first app

---
+++ lib/app/utils/style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static final TextStyle appbarStyle = TextStyle(
+      fontSize: 16, fontFamily: "Montserrat", fontWeight: FontWeight.w400);
+}
